3ed672e3c3eb243d068923e0197d97dc44276137
Administrator <slavbx@gmail.com>
2024-11-24 21:50:30 +0500

    Javadoc

--- src/main/java/org/slavbx/taskmanager/repository/RoleRepository.java
+++ src/main/java/org/slavbx/taskmanager/repository/RoleRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 /**
- * Репозиторий для хранения сущности роли {@link Role}.
+ * Репозиторий для хранения сущности роли {@link Role}
  */
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
